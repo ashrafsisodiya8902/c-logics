5a42d3fd73e1ee6a3c75ac1b9ed02e84fe6205c1
ashrafsisodiya8902 <147736312+ashrafsisodiya8902@users.noreply.github.com>
2023-11-01 18:10:39 +0530

    Add files via upload

---
+++ fibonacci in c.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a=0,b=1,c,n;
+	printf("enter the value:");
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++)
+	{
+		printf("%d\n",a);
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	return 0;
+}
